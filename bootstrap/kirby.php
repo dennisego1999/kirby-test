fa5899640d2ddb46b79793a14da699fd32df170b
samlinck <43540055+samlinck@users.noreply.github.com>
2025-02-04 12:43:35 +0100

    Bootstrap Kirby

---
+++ bootstrap/kirby.php
@@ -0,0 +1,17 @@
+<?php
+
+use Kirby\Cms\App as Kirby;
+new Kirby([
+'roots' => [
+            'index' => $public = dirname(__DIR__) . '/public',
+            'assets' => $public . '/assets',
+            'content' => dirname(__DIR__) .'/content',
+            'site'=> $site = dirname(__DIR__) .'/site',
+            'storage' => $storage = dirname(__DIR__) . '/storage',
+            'accounts' => $storage.'/accounts',
+            'cache' => $storage.'/cache',
+            'sessions' => $storage.'/sessions',
+            'logs' => $storage.'/logs',
+        ]
+]);
+
--- public/index.php
+++ public/index.php
@@ -1,19 +1,7 @@
 <?php
 
-include __DIR__ . '/../kirby/vendor/autoload.php';
+require_once dirname(__DIR__) . '/bootstrap.php';
 
+use Kirby\Cms\App as Kirby;
 
-$kirby = new Kirby([
-    'roots' => [
-        'index'    => __DIR__,
-        'base'     => $base = dirname(__DIR__),
-        'content'  => $base . '/content',
-        'site'     => $base . '/site',
-        'storage'  => $storage = $base . '/storage',
-        'accounts' => $storage . '/accounts',
-        'cache'    => $storage . '/cache',
-        'sessions' => $storage . '/sessions',
-    ]
-]);
-
-echo $kirby->render();
+echo Kirby::instance()->render();
